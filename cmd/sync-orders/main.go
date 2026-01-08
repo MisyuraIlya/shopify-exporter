2c0c133f6b383bcae2219d2cb03dd5fc5824e4bd
ilya.mi@digitrade.co.il <ilya.mi@digitrade.co.il>
2026-01-08 16:04:47 +0200

    add connection db

--- cmd/sync-orders/main.go
+++ cmd/sync-orders/main.go
@@ -1,11 +1,14 @@
 // Daily jon fetcher from api to shopify
 package main
 
-import "shopify-exporter/internal/config"
+import (
+	"shopify-exporter/internal/config"
+)
 
 func main() {
 	cfg, err := config.LoadForDailySync()
 	if err != nil {
 		return
 	}
+
 }
--- cmd/sync-to-shopify/main.go
+++ cmd/sync-to-shopify/main.go
@@ -1,11 +1,20 @@
 // every 5 min job take from shopify to api
 package main
 
-import "shopify-exporter/internal/config"
+import (
+	"shopify-exporter/internal/config"
+	"shopify-exporter/internal/infra/mysql"
+)
 
 func main() {
 	cfg, err := config.LoadForSyncOrder()
 	if err != nil {
 		return
 	}
+	db, err := mysql.New(cfg.Mysql)
+
+	if err != nil {
+		return
+	}
+
 }
